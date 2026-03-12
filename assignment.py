67ce26ff6530863db6dceb746f76e8351e564907
informeddeno <125739814+informeddeno@users.noreply.github.com>
2026-03-13 01:03:18 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,2 +1,25 @@
+""" Write a Python program that prints the following message:
+Hello AI Students """
 print("Hello AI Students")
+
+''' Create the following variables in Python:
+name
+age
+city '''
+
+name = Alphonce Makacha
+age = 26
+city = Mombasa
+
+""" Print them so the output looks like this:
+
+Name: John
+Age: 20
+City: Nairobi """
+
+print(name) /n print(age) /n print(city)
+
+
+
+
 
